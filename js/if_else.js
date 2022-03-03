f69ffc7dc727be733d07ddbba7d1c6b7e6c761f5
Arnold Gerry <arnaud.gerry@gmail.com>
2022-03-03 18:45:06 +0300

    ft: else if operator extensively used

--- js/if_else.js
+++ js/if_else.js
@@ -12,11 +12,22 @@
 //     console.log("Keep cool today!");
 // }
 
+// var temperature = 30;
+// if (temperature < 15) {
+//     console.log("Don't forget a jacket!");
+// } else if (temperature < 25) {
+//     console.log("Have a nice day :");
+// } else {
+//     console.log("Keep cool today!");
+// }
+
 var temperature = 30;
-if (temperature < 15) {
-    console.log("Don't forget a jacket!");
+if (temperature < 0) {
+    console.log("Brr! Don't freeze out there!");
+} else if (temperature < 5) {
+    console.log("don't forget a jacket!");
 } else if (temperature < 25) {
-    console.log("Have a nice day :");
+    console.log("have a nice day :");
 } else {
     console.log("Keep cool today!");
 }
