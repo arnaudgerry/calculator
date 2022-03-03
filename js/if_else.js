dda1dc4543140bb2b2aeb1719e7d12767d9613d8
Arnold Gerry <arnaud.gerry@gmail.com>
2022-03-03 20:14:16 +0300

    ft: further practise on else if

--- js/if_else.js
+++ js/if_else.js
@@ -21,13 +21,47 @@
 //     console.log("Keep cool today!");
 // }
 
-var temperature = 30;
-if (temperature < 0) {
-    console.log("Brr! Don't freeze out there!");
-} else if (temperature < 5) {
-    console.log("don't forget a jacket!");
-} else if (temperature < 25) {
-    console.log("have a nice day :");
-} else {
-    console.log("Keep cool today!");
+// var temperature = 30;
+// if (temperature < 0) {
+//     console.log("Brr! Don't freeze out there!");
+// } else if (temperature < 5) {
+//     console.log("don't forget a jacket!");
+// } else if (temperature < 25) {
+//     console.log("have a nice day :");
+// } else {
+//     console.log("Keep cool today!");
+// }
+
+// function fizzBuzz(number){
+//     console.log(i);
+// }
+
+// function fizzBuzz(number) {
+//     if (number%5 === 0) console.log("Buzz");
+//     else if (number%3 === 0 && number%5 === 0) console.log("FizzBuzz");
+//     else console.log(number);
+// }
+// fizzBuzz(15)
+
+// function fizzBuzz(number){
+//     if (number%3 === 0) console.log("Fizz");
+//     else if(number%5 === 0) console.log("Buzz");
+//     else console.log(number);
+// }
+// fizzBuzz(15)
+
+// function fizzBuzz(number){
+//     if (number%3 === 0 && number%5 === 0) console.log("FizzBuzz");
+//     else if (number%3 === 0) console.log("Fizz");
+//     else if (number%5 === 0) console.log("Buzz");
+//     else console.log(number);
+// }
+// fizzBuzz(15)
+
+function fizzBuzz(number) {
+    if (number%15 === 0) console.log("FizzBuzz");
+    else if (number%3 === 0) console.log("Fizz");
+    else if (number%5 === 0) console.log("Buzz");
+    else console.log(number);
 }
+fizzBuzz(15)
