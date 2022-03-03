74ee58885066c140712b801d3ee7f7641be56eb6
Arnold Gerry <arnaud.gerry@gmail.com>
2022-03-03 18:34:31 +0300

    ft: created basic conditions else_if

---
+++ js/if_else.js
@@ -0,0 +1,22 @@
+// var enoughSleep = true;
+// if (enoughSleep) {
+//     console.log("i am ready to go!");
+// } else {
+//     console.log("I need a nap");
+// }
+
+// var temperature = 30;
+// if (temperature < 20) {
+//     console.log("Don't forget a jacket!");
+// } else {
+//     console.log("Keep cool today!");
+// }
+
+var temperature = 30;
+if (temperature < 15) {
+    console.log("Don't forget a jacket!");
+} else if (temperature < 25) {
+    console.log("Have a nice day :");
+} else {
+    console.log("Keep cool today!");
+}
